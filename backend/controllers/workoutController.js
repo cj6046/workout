264ff786f17b479aa527218789a8577a6745e1c1
Christopher Jones <cdjones6046@gmail.com>
2022-07-17 17:06:10 -0400

    Created controllers folder
    - Created workoutController for CRUD operations
    - Implemented workoutController in workoutRouter
    - Successfully communicated to mongoDB

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,59 @@
+const workoutModel = require("../models/workoutModel");
+const mongoose = require("mongoose");
+
+// get all workouts
+const getAllWorkouts = async (req, res) => {
+  try {
+    const workouts = await workoutModel.find({});
+    res.status(200).json(workouts);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// get single workout
+const getWorkout = async (req, res) => {
+  try {
+    // pass props from endpoint through the request
+    const { id } = req.params;
+    // check validity of id
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: "Invalid workout ID" });
+    }
+    // retrieve from mongoose with model based on prop
+    const workout = await workoutModel.findById(id);
+    // check valid workout
+    if (!workout) {
+      return res.status(404).json({ error: "Workout not found" });
+    }
+    // return json data
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// create workout
+const createWorkout = async (req, res) => {
+  // Define vars for incoming body
+  const { title, reps, load } = req.body;
+
+  // Try/Catch T: Create document based on workoutModel
+  try {
+    const workout = await workoutModel.create({ title, reps, load });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// update workout
+
+// delete workout
+
+// export functions
+module.exports = {
+  getAllWorkouts,
+  getWorkout,
+  createWorkout,
+};
--- backend/routers/workoutRouter.js
+++ backend/routers/workoutRouter.js
@@ -1,34 +1,23 @@
 // requirements (express)
 const express = require("express");
-const workoutModel = require("../models/workoutModel");
+const {
+  getAllWorkouts,
+  getWorkout,
+  createWorkout,
+} = require("../controllers/workoutController");
 
 // create instance of Router()
 const router = express.Router();
 
 /*** REQUEST HANDLERS ***/
 // Get all workouts
-router.get("/", (req, res) => {
-  res.json({ mssg: "Get all workouts" });
-});
+router.get("/", getAllWorkouts);
 
 // Get individual workout
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "Get single workout" });
-});
+router.get("/:id", getWorkout);
 
 // Create a new workout
-router.post("/", async (req, res) => {
-  // Define vars for incoming body
-  const { title, reps, load } = req.body;
-
-  // Try/Catch T: Create document based on workoutModel
-  try {
-    const workout = await workoutModel.create({ title, reps, load });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+router.post("/", createWorkout);
 
 // Update a workout
 router.patch("/:id", (req, res) => {
