4a561a7f2148ae5efcc6cc8fc03c62f7dba6adac
Christopher Jones <cdjones6046@gmail.com>
2022-07-17 12:38:56 -0400

    Connected to MongoDB
    - Downloaded and required mongoose
    - Moved listening message into connection block

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 // requirements
 require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const workoutRouter = require("./routers/workouts");
 
@@ -21,7 +22,15 @@ app.get("/", (req, res) => {
 
 app.use("/api/workouts", workoutRouter);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listening on port", process.env.PORT);
-});
+// connect to database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db and listening on port", process.env.PORT);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
